3b1c8f678009c9f470ca97dc672440f85b7adf86
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-01-08 15:46:05 -0800

    tests

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,3 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class DynamicStringListTest {
-  
+  @Test
+  public void DynamicStringList_Get(){
+    DynamicStringList list = new DynamicStringList();
+    list.add("1");
+    list.add("2");
+    list.add("3");
+    list.add("4");
+
+
+    String result = list.get(0);
+    assertEquals("1", result);
+  }
 }
